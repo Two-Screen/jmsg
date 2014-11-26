c03b9a7d806e38d674e9d9d2af3e3ab594dca879
Stéphan Kochen <stephan@angrybytes.com>
2014-11-26 11:10:42 +0100

    Add stream test.

--- tests.js
+++ tests.js
@@ -1,3 +1,4 @@
+var net = require('net');
 var test = require('tap').test;
 var jmsg = require('./');
 
@@ -95,5 +96,44 @@ test('close', { timeout: 500 }, function(t) {
     pair.left.send('good', function(err) {
         t.is(err.message, 'Connection closed',
             'Callback receives error after close');
+    });
+});
+
+
+function createSocketPair(t, cb) {
+    var right;
+    var server = net.createServer(function(left) {
+        t.on('end', function() {
+            left.destroy();
+        });
+        cb(left, right);
+    });
+    t.on('end', function() {
+        server.close();
+    });
+    server.listen(0, function() {
+        right = net.connect(server.address().port);
+        t.on('end', function() {
+            right.destroy();
+        });
+    });
+}
+
+test('duplex streams', { timeout: 500 }, function(t) {
+    t.plan(2);
+
+    createSocketPair(t, function(leftSocket, rightSocket) {
+        var left = jmsg.stream(leftSocket);
+        var right = jmsg.stream(rightSocket);
+
+        left.timeout = right.timeout = 100;
+
+        right.handlers.beep = function(arg, cb) {
+            t.pass('Right side received action');
+            cb();
+        };
+        left.send('beep', function(err) {
+            t.ok(!err, 'Left side received reply');
+        });
     });
 });
